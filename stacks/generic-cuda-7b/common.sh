8bc042fb5ab63beb0bdba3733c077a14407bb825
JP Meijers <jp.meijers@canonical.com>
2025-02-14 16:01:27 +0200

    Add cuda components and stack

---
+++ stacks/generic-cuda-7b/common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+
+engine="$SNAP_COMPONENTS/$(snapctl get engine)"
+model="$SNAP_COMPONENTS/$(snapctl get model)"
+
+if [ ! -d "$model" ]; then
+    echo "Missing component: $model"
+    exit 1
+fi
+
+source "$model/init" # exports MODEL_FILE
+
+if [ ! -d "$engine" ]; then
+    echo "Missing component: $engine"
+    exit 1
+fi
+
+# For staged shared objects
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$engine/usr/lib/$ARCH_TRIPLET:$engine/usr/local/lib"
+
+# Other user changeable configs
+N_GPU_LAYERS="$(snapctl get n-gpu-layers | echo 33)" # TODO: Update to a good default for deepseek-r1-7b
