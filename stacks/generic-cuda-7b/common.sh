8332fbebfc5246b7b8b946bdb37decca49835e02
JP Meijers <jp.meijers@canonical.com>
2025-02-24 15:22:08 +0200

    Add cuda components & 7b and 70b stacks (#3)

---
+++ stacks/generic-cuda-7b/common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -eu
+
+engine="$SNAP_COMPONENTS/$(snapctl get engine)"
+model="$SNAP_COMPONENTS/$(snapctl get model)"
+
+if [ ! -d "$model" ]; then
+    echo "Missing component: $model"
+    exit 1
+fi
+
+source "$model/init" # exports MODEL_FILE
+
+if [ ! -d "$engine" ]; then
+    echo "Missing component: $engine"
+    exit 1
+fi
+
+# For staged shared objects
+export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:$engine/usr/lib/$ARCH_TRIPLET:$engine/usr/local/lib"
+
+# Other user changeable configs
+N_GPU_LAYERS="$(snapctl get n-gpu-layers || echo 29)" # Offload 29/29 layers to GPU
