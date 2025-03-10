fc2c46e2ed009b75dc6ebd338da64c8e936e429a
JP Meijers <jp.meijers@canonical.com>
2025-03-10 16:44:58 +0200

    Add a testflinger job and scripts

---
+++ test/scripts/connect-graphics.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -ex
+
+SNAP_NAME="deepseek-r1"
+
+sudo snap install mesa-2404
+sudo snap disconnect $SNAP_NAME:opengl || true
+sudo snap connect $SNAP_NAME:opengl
+sudo snap disconnect $SNAP_NAME:graphics || true
+sudo snap connect $SNAP_NAME:graphics mesa-2404:gpu-2404
