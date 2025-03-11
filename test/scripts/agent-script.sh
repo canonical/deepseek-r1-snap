53731cfb555e6c3d0512d34aed8af42fa29b2d38
JP Meijers <jp.meijers@canonical.com>
2025-03-11 12:42:26 +0200

    Try reconnecting opengl after switching stack

--- test/scripts/agent-script.sh
+++ test/scripts/agent-script.sh
@@ -5,27 +5,35 @@ declare -a stacks=("generic-cpu-1-5b" "generic-cuda-7b")
 
 echo "Testing: DEVICE_IP = $DEVICE_IP"
 
-echo "Installing drivers"
+# Don't refresh snaps automatically
+ssh ubuntu@$DEVICE_IP "sudo snap refresh --hold=3h --no-wait"
+
+# On UC22, the kernel, core, snapd snaps get refreshed right after first boot,
+# causing unexpected errors and triggering a reboot
+# On UC24, the auto refresh starts after a delay while testing
+echo "Force refresh snaps for consistency"
+ssh ubuntu@$DEVICE_IP "sudo snap refresh --no-wait"
+# Refresh of some system snaps stops SnapD and schedules a reboot
+while ! ssh ubuntu@$DEVICE_IP "$(< check-snap-changes.sh)"; do
+  echo "Waiting for snap changes ..."
+  sleep 30
+done
+
+echo "Installing NVIDIA drivers, CUDA and utils"
 ssh ubuntu@$DEVICE_IP "sudo apt-get -qqq update && \
 sudo apt-get -qqq install -y nvidia-driver-$NVIDIA_VERSION nvidia-utils-$NVIDIA_VERSION nvidia-cuda-toolkit"
 
-# Reboot the device in background to avoid breaking the SSH connection prematurely
+# Reboot the device to load NVIDIA drivers
+# In background to avoid breaking the SSH connection prematurely
 echo "Rebooting the device"
 ssh ubuntu@$DEVICE_IP "(sleep 3 && sudo reboot) &"
 
-# Wait for shutdown
+# Wait for shutdown to happen
 sleep 10
 
 # Wait for reboot
 while ! ssh ubuntu@$DEVICE_IP "ls"; do
   echo "Wait for reboot (ssh server online)..."
-  sleep 30
-done
-
-# On Ubuntu Core, kernel, core, snapd snaps get refreshed right after first boot,
-# causing unexpected errors and triggering a reboot
-while ! ssh ubuntu@$DEVICE_IP "$(< check-snap-changes.sh)"; do
-  echo "Wait for snap changes..."
   sleep 30
 done
 
@@ -35,25 +43,9 @@ ssh ubuntu@$DEVICE_IP "sudo snap install $SNAP_NAME --devmode --channel=$SNAP_CH
 echo "Refreshing $SNAP_NAME from $SNAP_CHANNEL"
 ssh ubuntu@$DEVICE_IP "sudo snap refresh $SNAP_NAME --channel=$SNAP_CHANNEL"
 
+# Temporary workaround while issue #7 is unresolved
 echo "Setting GPU layers"
-ssh ubuntu@$DEVICE_IP "sudo snap set $SNAP_NAME n-gpu-layers=30"
-
-# echo "Installing and reconnecting graphics interfaces"
-# ssh ubuntu@$DEVICE_IP "$(< connect-graphics.sh)"
-
-# It looks like we need to reboot after installing the snap for the graphics to be available
-# Reboot the device in background to avoid breaking the SSH connection prematurely
-echo "Rebooting the device"
-ssh ubuntu@$DEVICE_IP "(sleep 3 && sudo reboot) &"
-
-# Wait for shutdown
-sleep 10
-
-# Wait for reboot
-while ! ssh ubuntu@$DEVICE_IP "ls"; do
-  echo "Wait for reboot (ssh server online)..."
-  sleep 30
-done
+ssh ubuntu@$DEVICE_IP "sudo snap set $SNAP_NAME n-gpu-layers=29"
 
 # Loop through the array of stacks
 for i in "${stacks[@]}"
@@ -61,9 +53,12 @@ do
     echo ""
     echo "Testing stack $i"
     ssh ubuntu@$DEVICE_IP "sudo snap set $SNAP_NAME stack=\"$i\""
-    RESPONSE=$(ssh ubuntu@$DEVICE_IP "$(< device-script.sh)")
+
+    echo "Reconnecting opengl interface"
+    ssh ubuntu@$DEVICE_IP "$(< connect-graphics.sh)"
+
+    ssh ubuntu@$DEVICE_IP "$(< device-script.sh)"
     EXIT_CODE=$?
-    echo "$RESPONSE"
     if [ $EXIT_CODE -eq 0 ]; then
         echo "Stack $i succeeded"
     else
