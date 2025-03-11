2eb64fbd4c7c3e4ee453566b733235aa3752d616
JP Meijers <jp.meijers@canonical.com>
2025-03-11 12:42:26 +0200

    Try reconnecting opengl after switching stack

--- test/scripts/connect-graphics.sh
+++ test/scripts/connect-graphics.sh
@@ -2,8 +2,8 @@
 
 SNAP_NAME="deepseek-r1"
 
-sudo snap install mesa-2404
+# sudo snap install mesa-2404
 sudo snap disconnect $SNAP_NAME:opengl || true
 sudo snap connect $SNAP_NAME:opengl
-sudo snap disconnect $SNAP_NAME:graphics || true
-sudo snap connect $SNAP_NAME:graphics mesa-2404:gpu-2404
+# sudo snap disconnect $SNAP_NAME:graphics || true
+# sudo snap connect $SNAP_NAME:graphics mesa-2404:gpu-2404
