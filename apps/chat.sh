f7a68b076b36914fac5f432b08f1c931c7e6501b
JP Meijers <jp.meijers@canonical.com>
2025-06-20 11:45:16 +0000

    Change to http.base-path specified in all stacks Move chat error messaging to chat client

--- apps/chat.sh
+++ apps/chat.sh
@@ -4,22 +4,9 @@ port="$(snapctl get http.port)"
 model_name="$(snapctl get model-name)"
 
 # Normally the OpenAI API is hosted under http://server:port/v1. In some cases like with OpenVINO Model Server it is under http://server:port/v3
-api_base_path="$(snapctl get api-base-path)"
+api_base_path="$(snapctl get http.base-path)"
 if [ -z "$api_base_path" ]; then
   api_base_path="v1"
 fi
 
-set +e
-
 OPENAI_BASE_URL="http://localhost:$port/$api_base_path" MODEL_NAME="$model_name" REASONING_MODEL=true "$SNAP"/bin/go-chat-client
-status=$?
-
-set -e
-
-if [ $status -ne 0 ]; then
-  echo "Exit code: $status"
-  echo ""
-  echo "Unable to chat. Make sure the server is started successfully."
-fi
-
-exit $status
