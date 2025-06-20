eec5f16bda2f8e35c1b732578b37ca2c1ed78881
JP Meijers <jp.meijers@canonical.com>
2025-06-20 09:55:54 +0000

    Remove init from chat Keep exit on error and error on undefined vars in chat script

--- apps/chat.sh
+++ apps/chat.sh
@@ -1,18 +1,21 @@
-#!/bin/bash
-
-"$SNAP"/bin/init.sh
+#!/bin/bash -eu
 
 port="$(snapctl get http.port)"
 model_name="$(snapctl get model-name)"
 
-# Normally the OpenAI API is hosted under http://server:port/v1. In some cases like with OpenVINO it is under http://server:port/v3
+# Normally the OpenAI API is hosted under http://server:port/v1. In some cases like with OpenVINO Model Server it is under http://server:port/v3
 api_base_path="$(snapctl get api-base-path)"
 if [ -z "$api_base_path" ]; then
   api_base_path="v1"
 fi
 
-OPENAI_BASE_URL="http://localhost:$port/$api_base_path" MODEL_NAME="$model_name" REASONING_MODEL=True "$SNAP"/bin/go-chat-client
+set -e
+
+OPENAI_BASE_URL="http://localhost:$port/$api_base_path" MODEL_NAME="$model_name" REASONING_MODEL=true "$SNAP"/bin/go-chat-client
 status=$?
+
+set +e
+
 if [ $status -ne 0 ]; then
   echo "Exit code: $status"
   echo ""
