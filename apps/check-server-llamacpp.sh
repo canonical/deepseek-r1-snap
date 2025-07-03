607fd8015d6f2bcdb4b2afc26e79565cdc63affd
JP Meijers <jp.meijers@canonical.com>
2025-07-03 16:43:12 +0200

    Reuse llamacpp and openvino checks

--- apps/check-server-llamacpp.sh
+++ apps/check-server-llamacpp.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -eu
 
 port="$(snapctl get http.port)"
-model="$(snapctl get model)"
+model_name="$(snapctl get model-name)"
+api_base_path="$(snapctl get http.base-path)"
 
 # Check is server is started. - False negative when running in foreground. Will also not detect "failed".
 #service_status="$(snapctl services | grep "deepseek-r1.server" | awk '{print $3}')"
@@ -20,33 +21,36 @@ else
   exit 1
 fi
 
-#served_model=$(wget http://localhost:8080/v1/models -O- 2>/dev/null | jq .data[0].id)
-#if [[ "$served_model" == *"$model"* ]]; then
+#served_model=$(wget http://localhost:8080/$api_base_path/models -O- 2>/dev/null | jq .data[0].id)
+#if [[ "$served_model" == *"$model_name"* ]]; then
 #  echo "Expected model is being served"
 #else
 #  echo "Models endpoint not returning expected model"
 #  exit 1
 #fi
 
+request=$(printf '{"model": "%s", "prompt": "Say this is a test", "temperature": 0, "max_tokens": 1}' "$model_name")
 api_response=$(\
-  wget http://localhost:8080/v1/completions \
-  --post-data='{"model": "", "prompt": "Say this is a test", "temperature": 0, "max_tokens": 1}' \
+  wget http://localhost:8080/$api_base_path/completions \
+  --post-data="$request" \
   --content-on-error \
   -O- \
   2>/dev/null\
 )
 
 # Still starting up api_response = {"error":{"code":503,"message":"Loading model","type":"unavailable_error"}}
-error_text=$(echo $api_response | jq .error.message)
+error_text=$(echo "$api_response" | jq .error.message)
 if [ "${error_text}" != "null" ]; then
   echo "Server is not ready: $error_text"
   exit 1
 fi
 
-chat_text=$(echo $api_response | jq .choices[0].text)
+chat_text=$(echo "$api_response" | jq .choices[0].text)
 if [ -z "${chat_text}" ]; then
   echo "No response from completions api"
   exit 1
 else
   echo "Valid response from completions api"
 fi
+
+exec "$@"
---
+++ apps/check-server-openvino.sh
@@ -0,0 +1,62 @@
+#!/bin/bash -eu
+
+port="$(snapctl get http.port)"
+model_name="$(snapctl get model-name)"
+api_base_path="$(snapctl get http.base-path)"
+
+# Check is server is started. - False negative when running in foreground. Will also not detect "failed".
+#service_status="$(snapctl services | grep "deepseek-r1.server" | awk '{print $3}')"
+#if [ "$service_status" == "active" ]; then
+#  echo "Server is running"
+#else
+#  echo "Server is not started"
+#  exit 1
+#fi
+
+# Check if port is open and we can connect over TCP
+if (nc -z localhost $port 2>/dev/null); then
+  echo "Server is listening"
+else
+  echo "Server is not listening"
+  exit 1
+fi
+
+#served_model=$(wget http://localhost:8080/$api_base_path/models -O- 2>/dev/null | jq .data[0].id)
+#if [[ "$served_model" == *"$model_name"* ]]; then
+#  echo "Expected model is being served"
+#else
+#  echo "Models endpoint not returning expected model"
+#  exit 1
+#fi
+
+request=$(printf '{"model": "%s", "prompt": "Say this is a test", "temperature": 0, "max_tokens": 1}' "$model_name")
+api_response=$(\
+  wget http://localhost:8080/$api_base_path/completions \
+  --post-data="$request" \
+  --content-on-error \
+  -O- \
+  2>/dev/null\
+)
+
+# Still starting up api_response = "Mediapipe graph definition with requested name is not found"
+if [ "${api_response}" == "Mediapipe graph definition with requested name is not found" ]; then
+  echo "Server is not ready"
+  exit 1
+fi
+
+if ! (echo "$api_response" | jq -ne 0 2>/dev/null > /dev/null); then
+  echo "Server response is not json: $api_response"
+fi
+
+chat_text=$(echo "$api_response" | jq .choices[0].text)
+if [ "$chat_text" == null ]; then
+  echo "Invalid response: $api_response"
+  exit 1
+elif [ -z "${chat_text}" ]; then
+  echo "No response from completions api"
+  exit 1
+else
+  echo "Valid response from completions api"
+fi
+
+exec "$@"
