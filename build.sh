367f401710d2e4a14979164327ab695e151b436e
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-07-23 10:35:48 -0300

    feat: improve output progress reporting
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- build.sh
+++ build.sh
@@ -7,23 +7,22 @@ if [ -z "${1-}" ]; then
   exit 0
 fi
 
-echo "Stack selected: '$1'"
-
 STACK_NAME="${1-}"
+echo "Stack selected: '$STACK_NAME'"
+
 STACK_DIR="stacks/$STACK_NAME"
 STACK_FILE="$STACK_DIR/stack.yaml"
 if [ ! -d "$STACK_DIR" ]; then
-  echo "Error: Stack '$STACK_NAME' does not exist."
+  echo "Error: Stack '$STACK_NAME' does not exist." >&2
   exit 1
 fi
 
-echo "Extracting model and engine"
 stack_yaml=$(yq '. | explode(.)' "$STACK_FILE")
 readarray -t components < <(yq '.components[]' "$STACK_FILE")
 echo "Selected from stack ${STACK_NAME}: ${components[*]}"
 printf -v llm_pieces "%s|" "${components[@]}"
 
-echo -e "Generating new snapcraft.yaml\n"
+echo "Generating new snapcraft.yaml"
 essentials="app-scripts|stacks|ml-snap-utils|go-chat-client|common-runtime-dependencies"
 
 yq "explode(.) |
