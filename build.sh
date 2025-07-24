fae189310b76d3163d1527c76ac763d1024cc195
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-07-24 09:18:15 -0300

    Revert "feat: allow build without any components"
    This reverts commit 342616a650c88b1c0d53f32efc0d32e516c6cd6b.

--- build.sh
+++ build.sh
@@ -5,6 +5,13 @@ exit_error() {
   exit 1
 }
 
+# if no argument is provided, just build everything
+if [ -z "${1-}" ]; then
+  rm -f snapcraft.yaml
+  snapcraft -v
+  exit 0
+fi
+
 STACK_NAME="${1-}"
 echo "Stack selected: '$STACK_NAME'"
 
@@ -22,6 +29,11 @@ fi
 
 # Creates the components array with the contents of the .components[] list
 readarray -t components < <(yq '.components[]' "$STACK_FILE")
+
+# Check if array lenght is 0
+if [[ ${#components[@]} -eq 0 ]]; then
+  exit_error "Stack '$STACK_FILE' has no components"
+fi
 echo "Selected from stack ${STACK_NAME}: ${components[*]}"
 
 # Converts the array into a string separated by '|'
