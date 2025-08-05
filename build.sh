0d4a6c56fcc738d3ebb20eb03da85708074e67cf
Lincoln Wallace <lincoln.wallace@canonical.com>
2025-08-05 07:56:42 -0300

    feat: Completer script
    Signed-off-by: Lincoln Wallace <lincoln.wallace@canonical.com>

--- build.sh
+++ build.sh
@@ -50,7 +50,7 @@ echo "Stack components: ${components[*]}"
 printf -v llm_pieces "%s|" "${components[@]}"
 
 echo "Generating new snapcraft.yaml"
-essentials="app-scripts|stacks|ml-snap-utils|go-chat-client|common-runtime-dependencies|opencl-driver"
+essentials="app-scripts|stacks|ml-snap-utils|go-chat-client|common-runtime-dependencies|opencl-driver|completer"
 
 # Copy snap/snapcraft.yaml to snapcraft.yaml, retaining only the parts and components that match the regex
 yq "explode(.) |
