974e6d49a5ace8dd78be94558753c29bb14f69f7
JP Meijers <jp.meijers@canonical.com>
2025-08-19 14:37:53 +0200

    Check fail bc missing

--- test/stack-tps/agent.sh
+++ test/stack-tps/agent.sh
@@ -5,12 +5,16 @@ echo "DEVICE_IP = $DEVICE_IP"
 echo "SNAP_NAME = $SNAP_NAME"
 echo "SNAP_CHANNEL = $SNAP_CHANNEL"
 
-# retrieve the tools installer
+echo "Installing hwcert tools"
 curl -Ls -o install_tools.sh https://raw.githubusercontent.com/canonical/hwcert-jenkins-tools/main/install_tools.sh
 # install the scriptlets and other tools on the agent and the device, as necessary
 export TOOLS_PATH=tools
 source install_tools.sh $TOOLS_PATH
 [ ! "$?" -eq 0 ] && echo "Error: Failed to run installer" && exit 1
+
+echo "Installing agent dependencies"
+#sudo apt-get install --yes bc
+#sudo snap install jq
 
 # ensure device is available before continuing
 wait_for_ssh --allow-degraded || exit 1
@@ -25,10 +29,9 @@ echo "Force refresh snaps for consistency"
 _run sudo snap refresh --no-wait
 wait_for_snap_changes
 
-echo "Installing dependencies"
-_run sudo apt-get install --yes git bc
+echo "Installing device dependencies"
+_run sudo apt-get install --yes git
 _run sudo snap install go --classic --no-wait
-_run sudo snap install jq --no-wait
 
 echo "Remove $SNAP_NAME if already installed"
 _run sudo snap remove "$SNAP_NAME" --no-wait
@@ -41,24 +44,24 @@ selected_stack=$(_run sudo snap get deepseek-r1 stack)
 echo "Auto selected stack: $selected_stack"
 
 if [ "$EXPECTED_STACK" != "$selected_stack" ]; then
-  echo "WARNING: Expected stack not selected"
-  # exit 1
+  echo "Error: incorrect stack selected"
+  exit 1
 fi
+
+# Start the server. While we wait clone the benchmark tools. Then check if server has started.
+_run sudo snap start "$SNAP_NAME".server
+_run git clone https://github.com/jpm-canonical/llmapibenchmark.git
+_run snap run --shell "$SNAP_NAME" "/snap/$SNAP_NAME/current/bin/wait-for-server.sh"
 
 # If the model name option is set, use it when talking to api
 model_name=$(_run sudo snap get deepseek-r1 model-name || echo "")
-
-_run sudo snap start "$SNAP_NAME".server
-sleep 5
-_run snap run --shell "$SNAP_NAME" "/snap/$SNAP_NAME/current/bin/wait-for-server.sh"
-
-_run git clone https://github.com/jpm-canonical/llmapibenchmark.git
 benchmark_result=$(_run "cd llmapibenchmark/cmd && DEBUG=true go run . --base-url=http://localhost:8080/v1 --model=$model_name --concurrency=1 --format=json")
 echo "$benchmark_result"
 
 result_tps=$(echo "$benchmark_result" | jq .results[0].generation_speed)
+too_slow=$(echo "$result_tps < $EXPECTED_TPS" | bc -l)
 
-if (( $(echo "$result_tps < $EXPECTED_TPS" | bc -l) )); then
+if $too_slow; then
   echo "ERROR: Performance lower than expected: $result_tps < $EXPECTED_TPS"
   exit 1
 fi
--- test/stack-tps/run.sh
+++ test/stack-tps/run.sh
@@ -4,24 +4,24 @@ export SNAP_NAME="deepseek-r1"
 export SNAP_CHANNEL="latest/edge"
 
 # Values from matrix
-#export JOB_QUEUE="dell-xps-13-7390"
-#export EXPECTED_STACK="cpu"
-#export EXPECTED_TPS=1.5
+export JOB_QUEUE="dell-xps137390-c27366"
+export EXPECTED_STACK="cpu-avx512"
+export EXPECTED_TPS=3.5
 
 ## Ampere Altra
 #export JOB_QUEUE="202212-30936"
 #export EXPECTED_STACK="ampere-altra"
-#export EXPECTED_TPS=15
+#export EXPECTED_TPS=9
 
 ## Ampere Altra Max
 #export JOB_QUEUE="202303-31419"
 #export EXPECTED_STACK="ampere-altra"
 #export EXPECTED_TPS=15
 
-# Ampere One - puniper
-export JOB_QUEUE="202501-36266"
-export EXPECTED_STACK="ampere-one"
-export EXPECTED_TPS=8
+## Ampere One - puniper
+#export JOB_QUEUE="202501-36266"
+#export EXPECTED_STACK="ampere-one"
+#export EXPECTED_TPS=15
 
 envsubst < testflinger.yaml > testflinger.temp.yaml
 
