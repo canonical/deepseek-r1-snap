c7feab0ef19e8f67ae96fe71d780adcd9179f8fe
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2025-10-08 15:13:52 +0200

    Refactor, use time to track, stop failing serivce

--- apps/server.sh
+++ apps/server.sh
@@ -1,40 +1,48 @@
-#!/bin/bash -eu
+#!/bin/bash -ue
 
 check_missing_components() {
-    local -n missing_ref=$1
-    local required_components="$2"
+    local -n missing=$1 # nameref for output array
+    local required="$2"
 
-    missing_ref=()
-    for component in $required_components; do
+    missing=()
+    for component in $required; do
         if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
-            missing_ref+=("$component")
+            missing+=("$component")
         fi
     done
 }
 
-engine_manifest=$(deepseek-r1 show-engine)
-engine="$(echo "$engine_manifest" | yq .name)"
-required_components="$(echo "$engine_manifest" | yq .components[])"
-unset engine_manifest
+wait_for_components() {
+    local required_components
+    
+    required_components="$(deepseek-r1 show-engine | yq .components[])"
 
-# Check for missing components
-missing_components=()
-check_missing_components missing_components "$required_components"
-
-retry_count=0
-delay=10
-max_retries=360 # 10 seconds * 360 = 1 hour
-while [ ${#missing_components[@]} -ne 0 ] && [ "$retry_count" -lt $max_retries ]; do
-    echo "Missing required snap components: [${missing_components[*]}], retrying in $delay seconds..."
-    sleep "$delay"
-    ((++retry_count))
+    missing_components=()
     check_missing_components missing_components "$required_components"
-done
 
-if [ ${#missing_components[@]} -ne 0 ]; then
-    echo "Error: timed out after $max_retries retries, missing components [${missing_components[*]}]."
-    exit 1
-fi
+    max=20 # seconds
+    interval=10
+    elapsed=0
+    while [ ${#missing_components[@]} -ne 0 ] && [ "$elapsed" -lt $max ]; do
+        ((elapsed+=interval))
+        echo "Missing required snap components: [${missing_components[*]}] ($elapsed/${max}s)"
+        sleep "$interval"
+        
+        check_missing_components missing_components "$required_components"
+    done
 
+    if [ ${#missing_components[@]} -ne 0 ]; then
+        echo "Error: timed out after ${elapsed}s while waiting for required components: [${missing_components[*]}]"
+        echo "Please use \"snap changes\" to monitor the progress and start the service once all components are installed."
+        
+        # Stop service to avoid indefinite retries by systemd, until the next reboot
+        snapctl stop deepseek-r1
+        
+        exit 1
+    fi
+}
 
+wait_for_components
+
+engine="$(deepseek-r1 show-engine | yq .name)"
 exec "$SNAP/engines/$engine/server" "$@"
