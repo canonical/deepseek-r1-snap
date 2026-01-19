57ed41dd1d2604818b0d0caa3285d6595531e6dd
JP Meijers <jp.meijers@canonical.com>
2026-01-19 10:07:09 +0200

    Bump cli to beta.27

--- apps/server.sh
+++ apps/server.sh
@@ -1,48 +1,11 @@
 #!/bin/bash -eu
 
-check_missing_components() {
-    local -n missing=$1 # nameref for output array
-    local required="$2"
+# Save the status for content sharing
+# This must be done each time the server is started to expose the actual status
+mkdir -p "$STATUS_SHARE"
+# Wait for component before querying status
+modelctl run true --wait-for-components && \
+    modelctl status --format=json > "$STATUS_SHARE/status.json"
 
-    missing=()
-    for component in $required; do
-        if [ ! -d "$SNAP_COMPONENTS/$component" ]; then
-            missing+=("$component")
-        fi
-    done
-}
-
-wait_for_components() {
-    local required_components
-    
-    required_components="$(deepseek-r1 show-engine | yq .components[])"
-
-    missing_components=()
-    check_missing_components missing_components "$required_components"
-
-    max=3600 # seconds
-    interval=10
-    elapsed=0
-    while [ ${#missing_components[@]} -ne 0 ] && [ "$elapsed" -lt $max ]; do
-        ((elapsed+=interval))
-        echo "Waiting for required snap components: [${missing_components[*]}] ($elapsed/${max}s)"
-        sleep "$interval"
-        
-        check_missing_components missing_components "$required_components"
-    done
-
-    if [ ${#missing_components[@]} -ne 0 ]; then
-        echo "Error: timed out after ${elapsed}s while waiting for required components: [${missing_components[*]}]"
-        echo "Please use \"snap changes\" to monitor the progress and start the service once all components are installed."
-        
-        # Stop service to avoid indefinite retries by systemd, until the next reboot
-        snapctl stop deepseek-r1
-        
-        exit 1
-    fi
-}
-
-wait_for_components
-
-engine="$(deepseek-r1 show-engine | yq .name)"
-exec "$SNAP/engines/$engine/server" "$@"
+engine="$(modelctl show-engine | yq .name)"
+modelctl run "$SNAP/engines/$engine/server" --wait-for-components
