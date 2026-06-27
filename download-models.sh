841f1b591968ac39d46e8e834f68e3fa93c8cdb6
Farshid Tavakolizadeh <farshid.tavakolizadeh@canonical.com>
2026-06-27 06:12:45 +0200

    Remove symlink creation for mediapipe graph
    This is done also at build time.

--- download-models.sh
+++ download-models.sh
@@ -22,13 +22,9 @@ hf download bartowski/DeepSeek-R1-Distill-Qwen-7B-GGUF \
 # OpenVINO DeepSeek R1 Distill Qwen 7B INT4 (Intel CPU/GPU)
 hf download llmware/DeepSeek-R1-Distill-Qwen-7B-ov-int4 \
     --local-dir components/model-distill-qwen-7b-ov-int4/DeepSeek-R1-Distill-Qwen-7B-ov-int4
-# Make symlink for mediapipe graph to make it writable by OVMS when moved to a snap component
-ln -sf /tmp/graph.pbtxt components/model-distill-qwen-7b-ov-int4/DeepSeek-R1-Distill-Qwen-7B-ov-int4/graph.pbtxt
 
 # OpenVINO DeepSeek R1 Distill Qwen 7B INT4 (Intel NPU)
 hf download llmware/DeepSeek-R1-Distill-Qwen-7B-ov-int4-npu \
     --local-dir components/model-distill-qwen-7b-ov-int4-npu/DeepSeek-R1-Distill-Qwen-7B-ov-int4-npu
-# Make symlink for mediapipe graph to make it writable by OVMS when moved to a snap component
-ln -sf /tmp/graph.pbtxt components/model-distill-qwen-7b-ov-int4-npu/DeepSeek-R1-Distill-Qwen-7B-ov-int4-npu/graph.pbtxt
 
 echo "All models downloaded successfully!"
